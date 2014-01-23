fee4cb91b13953a9663ed88a7af3c4004136071f
dan-lyn <daniel_lynge@hotmail.com>
2014-01-23 15:47:04 +0100

    base html

---
+++ d7_boilerplate/html.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+  </head>
+  <body>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+    <?php print $scripts; ?>
+  </body>
+</html>
