2ae2b85b33ad1d606e28b5f78a748c57f6d6a507
dan-lyn <daniel_lynge@hotmail.com>
2014-02-07 19:14:04 +0100

    add meta viewport

--- d7_boilerplate/template.php
+++ d7_boilerplate/template.php
@@ -2,7 +2,17 @@
 
 function d7_boilerplate_preprocess_html(&$variables)
 {
-     //drupal_add_css(path_to_theme().'/css/style.css');
-     //drupal_add_js(path_to_theme().'/js/script.js');
+    $meta_viewport = array(
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => array(
+            'content' => 'width=device-width',
+            'name' => 'viewport',
+        )
+    );
+    drupal_add_html_head($meta_viewport, 'meta_viewport');
+
+    //drupal_add_css(path_to_theme().'/css/style.css');
+    //drupal_add_js(path_to_theme().'/js/script.js');
 }
 
