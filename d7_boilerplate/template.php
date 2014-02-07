98575e4ff7fcd54199c1bd53ce631f116357fda5
dan-lyn <daniel_lynge@hotmail.com>
2014-02-07 13:36:46 +0100

    example of template hook usage: template_preprocess_html

--- d7_boilerplate/template.php
+++ d7_boilerplate/template.php
@@ -1,3 +1,8 @@
 <?php
 
-// TODO
+function d7_boilerplate_preprocess_html(&$variables)
+{
+     //drupal_add_css(path_to_theme().'/css/style.css');
+     //drupal_add_js(path_to_theme().'/js/script.js');
+}
+
