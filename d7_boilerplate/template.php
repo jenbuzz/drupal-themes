0cf9118c34b5934b1953d65078347af26c560082
dan-lyn <daniel_lynge@hotmail.com>
2014-02-11 22:13:45 +0100

    code documentation

--- d7_boilerplate/template.php
+++ d7_boilerplate/template.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Implements hook_html_head_alter()
+ */
 function d7_boilerplate_html_head_alter(&$head_elements)
 {
     $head_elements['system_meta_content_type']['#attributes'] = array(
@@ -7,6 +10,9 @@ function d7_boilerplate_html_head_alter(&$head_elements)
     );
 }
 
+/**
+ * Implements template_preprocess_html()
+ */
 function d7_boilerplate_preprocess_html(&$variables)
 {
     $meta_viewport = array(
