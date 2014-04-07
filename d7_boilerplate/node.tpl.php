34808eae59fa3e8e6a166c7897b320a96bbd60dd
dan-lyn <daniel_lynge@hotmail.com>
2014-04-07 09:57:03 +0200

    removed unused id attribute

--- d7_boilerplate/node.tpl.php
+++ d7_boilerplate/node.tpl.php
@@ -1,4 +1,4 @@
-<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <header>
     <h1<?php print $title_attributes; ?>>
       <?php if (!$page): ?>
