bbbb98f9aaa1d5b95a6cd2764150a1841f63c668
dan-lyn <daniel_lynge@hotmail.com>
2014-05-21 20:17:08 +0200

    basic styling of login box

--- businesscard/template.php
+++ businesscard/template.php
@@ -1,2 +1,13 @@
 <?php
 
+function businesscard_form_alter(&$form, &$form_state, $form_id) {
+    
+  if (strstr($form_id, 'user_login')) {
+    $form['name']['#attributes']['class'][] = 'form-login-input';
+    $form['name']['#description'] = false;
+    $form['pass']['#attributes']['class'][] = 'form-login-input';
+    $form['pass']['#description'] = false;
+    $form['actions']['submit']['#attributes']['class'][] = 'form-login-submit';
+  }
+    
+}
