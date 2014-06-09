77a4d9e536f45be54217687f3aaa0333edcd386b
dan-lyn <daniel_lynge@hotmail.com>
2014-06-09 10:49:29 +0200

    setup image links under portfolio

--- businesscard/templates/page--front.tpl.php
+++ businesscard/templates/page--front.tpl.php
@@ -40,7 +40,10 @@
       <div id="portfolio" class="contentbox" style="background: #001F3F; color: #FFFFFF;">
         <div>
           <h2>portfolio</h2>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          <ul>
+              <li><a href="#"><img src="sites/all/themes/businesscard/images/portfolio1.png" /></a></li>
+              <li><a href="#"><img src="sites/all/themes/businesscard/images/portfolio2.png" /></a></li>
+            </ul>
         </div>
       </div>
 
