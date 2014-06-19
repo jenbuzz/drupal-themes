ab7e4fac9ca14db1560dd38d1f98e7fc45bd251d
dan-lyn <daniel_lynge@hotmail.com>
2014-06-19 13:05:29 +0200

    link to top of page

--- businesscard/templates/page--front.tpl.php
+++ businesscard/templates/page--front.tpl.php
@@ -25,21 +25,21 @@
 
       <div id="about" class="contentbox" style="background: #FF4136; color: #FFFFFF;">
         <div>
-          <h2>about</h2>
+          <h2>about <a href="#page"><span class="fa fa-level-up"></span></a></h2>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
 
       <div id="work" class="contentbox" style="background: #85144B; color: #FFFFFF;">
         <div>
-          <h2>work profile</h2>
+          <h2>work profile <a href="#page"><span class="fa fa-level-up"></span></a></h2>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
 
       <div id="portfolio" class="contentbox" style="background: #001F3F; color: #FFFFFF;">
         <div>
-          <h2>portfolio</h2>
+          <h2>portfolio <a href="#page"><span class="fa fa-level-up"></span></a></h2>
           <ul>
               <li><a href="#"><img src="sites/all/themes/businesscard/images/portfolio1.png" /></a></li>
               <li><a href="#"><img src="sites/all/themes/businesscard/images/portfolio2.png" /></a></li>
@@ -49,7 +49,7 @@
 
       <div id="contact" class="contentbox" style="background: #B10BC9; color: #FFFFFF;">
         <div>
-          <h2>contact</h2>
+          <h2>contact <a href="#page"><span class="fa fa-level-up"></span></a></h2>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
