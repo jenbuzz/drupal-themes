11505fcad3585c98cfc26dc51b7a94c683ffdbee
dan-lyn <daniel_lynge@hotmail.com>
2014-08-18 19:56:39 +0200

    added drupal.behavior default code

--- d7_boilerplate/js/script.js
+++ d7_boilerplate/js/script.js
@@ -1,1 +1,14 @@
-// add javascript here
+(function ($, Drupal, window, document, undefined) {
+
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+
+    // replace my_custom_behavior with your own title
+
+    // insert javascript here...
+
+  }
+};
+
+})(jQuery, Drupal, this, this.document);
+
